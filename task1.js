59d496b972f30438a1e6ddd27a0ed067dc27fc17
KaterynaKorniienko <katerynakorniienko7@ukr.net>
2021-12-29 17:15:48 +0200

    first commit

---
+++ task1.js
@@ -0,0 +1,13 @@
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user["premium"] = false;
+// console.log(user)
+for(let key of Object.keys(user) ){
+    console.log(key + ': '+ user[key]);
+}
